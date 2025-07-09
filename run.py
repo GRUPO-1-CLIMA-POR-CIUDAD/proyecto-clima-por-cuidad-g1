7acde5878613a805a9151155e2b5ad99e93e8dbb
tracy200319 <tracynicolehmorianotuanama@gmail.com>
2025-07-09 03:23:54 -0500

    Update

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # PORT lo asigna Render
+    app.run(host='0.0.0.0', port=port)
